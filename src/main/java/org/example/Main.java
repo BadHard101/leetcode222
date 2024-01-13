2d5bec5419088ec81d0309264525cd7aa2be2780
BadHard <BadHard101@yandex.ru>
2024-01-13 20:13:52 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
